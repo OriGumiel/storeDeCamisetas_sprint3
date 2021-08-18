e70ab5729ed8feab74f74e980609bcf607525d7b
DiegoFGuevara <dfguevarag@gmail.com>
2021-08-18 18:03:22 -0300

    Formulario crear y editar

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,7 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 
-const productsController = require ('../controllers/productsController')
+const productsController = require ('../controllers/productsController');
+
+const multer = require('multer');
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname + '../../public/images/products'))
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    }
+  }) 
+  let upload = multer({ storage: storage })
+
 
 /* GET users listing. */
 router.get('/', productsController.todos );
@@ -9,6 +22,11 @@ router.get('/', productsController.todos );
 router.get('/detalle/:id',productsController.detalle );
 
 router.get('/nuevoProducto',productsController.nuevo );
+router.post('/store', upload.single('images'),productsController.store); 
+
+/*** EDIT ONE PRODUCT ***/ 
+router.get('/edit/:id/', productsController.edit); 
+router.put('/edit/:id', productsController.update); 
 
 router.get('/carrito',productsController.cart );
 
