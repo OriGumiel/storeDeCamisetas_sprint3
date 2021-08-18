112e38cb083aefbbe29b5e7ff61a3edcf261c0f9
DiegoFGuevara <dfguevarag@gmail.com>
2021-08-18 10:04:23 -0300

    actulizacion CRUD

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,3 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
+
 const productsController = {
     todos: function(req, res, next) {
         res.render('products/productsAll',{title: 'todos los productos'});
@@ -10,10 +18,30 @@ const productsController = {
       },
 
     nuevo: function(req, res, next) {
-        res.render('products/productsNew',{title: 'Creaste un nuevo producto'});
+        res.render('products/productsNew',{title: 'Creaste un nuevo producto',
+        style: '/stylesheets/styles.css',
+      });
       },
 
-    cart: function(req, res, next) {
+    store: (req, res) => {
+        
+        let nuevoId = products[products.length - 1].id + 1;
+        
+        let nuevoProducto = {
+            name: req.body.name,
+            price: req.body.price,
+            category: req.body.category,
+            discount: req.body.discount,
+            id: nuevoId,
+            image: req.file.originalname,
+        }
+
+        products.push(nuevoProducto);
+        fs.writeFileSync(productsFilePath, JSON.stringify(products));
+        res.redirect('/')
+    },
+
+     cart: function(req, res, next) {
        
         res.render('products/productsCart',{
           title: 'Estas en el carrito de productos',
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,7 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 
 const productsController = require ('../controllers/productsController')
+
+const multer = require('multer');
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname + '../../public/images/products'))
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    }
+  }) 
+  let upload = multer({ storage: storage })
+  
+
 
 /* GET users listing. */
 router.get('/', productsController.todos );
@@ -9,7 +23,15 @@ router.get('/', productsController.todos );
 router.get('/detalle/:id',productsController.detalle );
 
 router.get('/nuevoProducto',productsController.nuevo );
+router.post('/store', upload.single('images'),productsController.store); 
 
 router.get('/carrito',productsController.cart );
+
+//router.get('/detail/:id/', productsController.detail); 
+
+/*** EDIT ONE PRODUCT ***/ 
+//router.get('/edit/:id/', productsController.edit); 
+//router.put('/edit/:id', productsController.update); 
+
 
 module.exports = router;
