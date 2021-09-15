bb21d63813447088eb7e56bb0ebd21d189f2bf75
lucas-digitalhouse <lucaschiarizia@PDMC02D2DSMP3XY.local>
2021-09-14 21:18:53 -0300

    14/09

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -13,7 +13,31 @@ const usersController = {
         });
 
       },
-
+      create: (req, res) => {
+          let user = {
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email,
+            password: req.body.password, //bcrypt.hashSync(req.body.password, 10),
+            user_avatar:  req.file ? req.file.filename : '',
+            // role: 1
+          }
+          let archivoUsers = fs.readFileSync(path.resolve(__dirname, '../data/usuarios.json'), {
+            encoding: 'utf-8'
+          });
+          let users;
+          if (archivoUsers == "") {
+            users = [];
+          } else {
+            users = JSON.parse(archivoUsers);
+          };
+    
+          users.push(user);
+          usersJSON = JSON.stringify(users, null, 2);
+          fs.writeFileSync(path.resolve(__dirname, '../data/usuarios.json'), usersJSON);
+          res.redirect('/userlogin.ejs');
+          
+        }
 }
 
 module.exports = usersController;
