aeacd6c3b6c2366c3da642cec5e87eced9d05e24
Lucas Chiarizia <lucaschiarizia@PDMC02D2DSMP3XY.local>
2021-11-10 00:07:44 -0300

    arreglo de script de populacion probado

--- Store-Backend-Node.js/src/data/Script de populacion .sql
+++ Store-Backend-Node.js/src/data/Script de populacion .sql
@@ -1,23 +1,23 @@
-insert into Products (id, name, description, price, category, available_stock_id) values (1, 'Barcelona', 'Camiseta Nike Barcelona Home 2021/2022 + Parche UEFA Champions League', '$15500', 'esnovedad', 1);
-insert into Products (id, name, description, price, category, available_stock_id) values (2, 'España Retro', 'Camiseta Adidas España Retro Home 1998/1999 Dorsal 10 - Raul', '$15000', 'retro', 2);
-insert into Products (id, name, description, price, category, available_stock_id) values (3, 'PSG', 'Camiseta Nike PSG Home 2021/2022', '$14500', 'esnovedad', 3);
-insert into Products (id, name, description, price, category, available_stock_id) values (4, 'Alemania', 'Camiseta Adidas Alemania Home 2020/2021 + Parche Eurocopa', '$13000', 'selecciones', 4);
-insert into Products (id, name, description, price, category, available_stock_id) values (5, 'Ajax Retro', 'Camiseta Adidas Ajax Home 1994/1995', '$15500', 'retro', 5);
-insert into Products (id, name, description, price, category, available_stock_id) values (6, 'River', 'Camiseta Adidas River Home 2021/2022', '$11500', 'argentina', 6);
-insert into Products (id, name, description, price, category, available_stock_id) values (7, 'Liverpool', 'Camiseta Nike Liverppol Home 2021/2022 + Parche UEFA Champions League', '$15500', 'europa', 7);
-insert into Products (id, name, description, price, category, available_stock_id) values (8, 'España', 'Camiseta Adidas España Home 2021/2022', '$13000', 'selecciones', 8);
-insert into Products (id, name, description, price, category, available_stock_id) values (9, 'Boca', 'Camiseta Adidas Boca Home 2021/2022', '$11500', 'argentina', 9);
-insert into Products (id, name, description, price, category, available_stock_id) values (10, 'Suecia Retro', 'Camiseta Adidas Suecia Retro Home 1996/1998 Dorsal 7 - Larsson', '$15000', 'Comedy|Drama|Romance', 10);
-insert into Products (id, name, description, price, category, available_stock_id) values (11, 'Inter', 'Camiseta Nike Inter Home 2020/2021', '$13500', 'europa', 11);
-insert into Products (id, name, description, price, category, available_stock_id) values (12, 'Holanda Retro', 'Camiseta Nike Holanda Home 1994 Dorsal 10 - Bergkamp', '$15000', 'retro', 12);
-insert into Products (id, name, description, price, category, available_stock_id) values (13, 'Racing', 'Camiseta Kappa Racing Home 2021/2022', '$10000', 'argentina', 13);
-insert into Products (id, name, description, price, category, available_stock_id) values (14, 'Francia', 'Camiseta Nike Francia Home Vapor Match 2020/2021', '$16500', 'europa', 14);
-insert into Products (id, name, description, price, category, available_stock_id) values (15, 'Croacia', 'Camiseta Nike Croacia Away 2020/2021', '$13000', 'europa', 15);
-insert into Products (id, name, description, price, category, available_stock_id) values (16, 'Manchester City', 'Camiseta Puma Manchester City Home 2021/2022', '$14500', 'europa', 16);
-insert into Products (id, name, description, price, category, available_stock_id) values (17, 'Independiente', 'Camiseta Puma Independiente Home 2021/2022', '$10000', 'argentina', 17);
-insert into Products (id, name, description, price, category, available_stock_id) values (18, 'Holanda', 'Camiseta Nike Holanda Home Vapor Match 2021/2022', '$16500', 'selecciones', 18);
-insert into Products (id, name, description, price, category, available_stock_id) values (19, 'Real Madrid', 'Camiseta Adidas Real Madrid Home 2021/2022', '$14500', 'europa', 19);
-insert into Products (id, name, description, price, category, available_stock_id) values (20, 'Juventus', 'Camiseta Adidas Juventus Home 2021/2022', '$13500', 'europa', 20);
+insert into Products (id, name, description, price, category, available_stock_id) values (1, 'Barcelona', 'Camiseta Nike Barcelona Home 2021/2022 + Parche UEFA Champions League', '15500', 'europa', 1);
+insert into Products (id, name, description, price, category, available_stock_id) values (2, 'España Retro', 'Camiseta Adidas España Retro Home 1998/1999 Dorsal 10 - Raul', '15000', 'retro', 2);
+insert into Products (id, name, description, price, category, available_stock_id) values (3, 'PSG', 'Camiseta Nike PSG Home 2021/2022', '14500', 'europa', 3);
+insert into Products (id, name, description, price, category, available_stock_id) values (4, 'Alemania', 'Camiseta Adidas Alemania Home 2020/2021 + Parche Eurocopa', '13000', 'selecciones', 4);
+insert into Products (id, name, description, price, category, available_stock_id) values (5, 'Ajax Retro', 'Camiseta Adidas Ajax Home 1994/1995', '15500', 'retro', 5);
+insert into Products (id, name, description, price, category, available_stock_id) values (6, 'River', 'Camiseta Adidas River Home 2021/2022', '11500', 'argentina', 6);
+insert into Products (id, name, description, price, category, available_stock_id) values (7, 'Liverpool', 'Camiseta Nike Liverppol Home 2021/2022 + Parche UEFA Champions League', '15500', 'europa', 7);
+insert into Products (id, name, description, price, category, available_stock_id) values (8, 'España', 'Camiseta Adidas España Home 2021/2022', '13000', 'selecciones', 8);
+insert into Products (id, name, description, price, category, available_stock_id) values (9, 'Boca', 'Camiseta Adidas Boca Home 2021/2022', '11500', 'argentina', 9);
+insert into Products (id, name, description, price, category, available_stock_id) values (10, 'Suecia Retro', 'Camiseta Adidas Suecia Retro Home 1996/1998 Dorsal 7 - Larsson', '15000', 'retro', 10);
+insert into Products (id, name, description, price, category, available_stock_id) values (11, 'Inter', 'Camiseta Nike Inter Home 2020/2021', '13500', 'europa', 11);
+insert into Products (id, name, description, price, category, available_stock_id) values (12, 'Holanda Retro', 'Camiseta Nike Holanda Home 1994 Dorsal 10 - Bergkamp', '15000', 'retro', 12);
+insert into Products (id, name, description, price, category, available_stock_id) values (13, 'Racing', 'Camiseta Kappa Racing Home 2021/2022', '10000', 'argentina', 13);
+insert into Products (id, name, description, price, category, available_stock_id) values (14, 'Francia', 'Camiseta Nike Francia Home Vapor Match 2020/2021', '16500', 'europa', 14);
+insert into Products (id, name, description, price, category, available_stock_id) values (15, 'Croacia', 'Camiseta Nike Croacia Away 2020/2021', '13000', 'europa', 15);
+insert into Products (id, name, description, price, category, available_stock_id) values (16, 'Manchester City', 'Camiseta Puma Manchester City Home 2021/2022', '14500', 'europa', 16);
+insert into Products (id, name, description, price, category, available_stock_id) values (17, 'Independiente', 'Camiseta Puma Independiente Home 2021/2022', '10000', 'argentina', 17);
+insert into Products (id, name, description, price, category, available_stock_id) values (18, 'Holanda', 'Camiseta Nike Holanda Home Vapor Match 2021/2022', '16500', 'selecciones', 18);
+insert into Products (id, name, description, price, category, available_stock_id) values (19, 'Real Madrid', 'Camiseta Adidas Real Madrid Home 2021/2022', '14500', 'europa', 19);
+insert into Products (id, name, description, price, category, available_stock_id) values (20, 'Juventus', 'Camiseta Adidas Juventus Home 2021/2022', '13500', 'europa', 20);
 
 
 
@@ -104,7 +104,7 @@ insert into Product_images (id, image, product_id) values (66, 'croacia5.jpeg', 
 
 insert into Product_images (id, image, product_id) values (67, 'city.jpeg', 16);
 insert into Product_images (id, image, product_id) values (68, 'city2.jpeg', 16);
-insert into Product_images (id, image, product_id) values (68, 'city3.jpeg', 16);
+insert into Product_images (id, image, product_id) values (101, 'city3.jpeg', 16);
 insert into Product_images (id, image, product_id) values (69, 'city4.jpeg', 16);
 insert into Product_images (id, image, product_id) values (70, 'city5.jpeg', 16);
 
@@ -167,49 +167,49 @@ insert into Product_Shop_cart (id, product_id, shop_cart_id) values (20, 20, 20)
 
 
 
-insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (1, '$24752.36', 1, '4/10/2021', '12/16/2020', 1);
-insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (2, '$15063.56', 2, '4/2/2021', '7/6/2021', 1);
-insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (3, '$5688.04', 3, '5/6/2021', '8/27/2021', 1);
-insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (4, '$1208.27', 4, '8/30/2021', '12/5/2020', 1);
-insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (5, '$26094.41', 5, '1/19/2021', '8/15/2021', 1);
-insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (6, '$5706.71', 6, '9/17/2021', '9/28/2021', 1);
-insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (7, '$14657.93', 7, '1/17/2021', '6/7/2021', 1);
-insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (8, '$17604.27', 8, '10/29/2020', '5/1/2021', 1);
-insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (9, '$27972.88', 9, '6/6/2021', '6/22/2021', 1);
-insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (10, '$2428.71', 10, '4/14/2021', '6/2/2021', 1);
-insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (11, '$22126.28', 11, '4/13/2021', '6/10/2021', 1);
-insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (12, '$17143.28', 12, '8/27/2021', '11/17/2020', 1);
-insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (13, '$19908.83', 13, '8/18/2021', '9/11/2021', 1);
-insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (14, '$10343.34', 14, '10/15/2021', '7/30/2021', 1);
-insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (15, '$11388.11', 15, '10/20/2020', '12/29/2020', 1);
-insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (16, '$3339.99', 16, '11/16/2020', '12/7/2020', 1);
-insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (17, '$1969.87', 17, '12/11/2020', '5/31/2021', 1);
-insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (18, '$14301.74', 18, '3/29/2021', '9/29/2021', 1);
-insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (19, '$15476.54', 19, '6/18/2021', '3/11/2021', 1);
-insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (20, '$24550.47', 20, '12/1/2020', '9/8/2021', 1);
+insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (1, '24752.36', 1, '4/10/2021', '12/16/2020', 1);
+insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (2, '15063.56', 2, '4/2/2021', '7/6/2021', 1);
+insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (3, '5688.04', 3, '5/6/2021', '8/27/2021', 1);
+insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (4, '1208.27', 4, '8/30/2021', '12/5/2020', 1);
+insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (5, '26094.41', 5, '1/19/2021', '8/15/2021', 1);
+insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (6, '5706.71', 6, '9/17/2021', '9/28/2021', 1);
+insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (7, '14657.93', 7, '1/17/2021', '6/7/2021', 1);
+insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (8, '17604.27', 8, '10/29/2020', '5/1/2021', 1);
+insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (9, '27972.88', 9, '6/6/2021', '6/22/2021', 1);
+insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (10, '2428.71', 10, '4/14/2021', '6/2/2021', 1);
+insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (11, '22126.28', 11, '4/13/2021', '6/10/2021', 1);
+insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (12, '17143.28', 12, '8/27/2021', '11/17/2020', 1);
+insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (13, '19908.83', 13, '8/18/2021', '9/11/2021', 1);
+insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (14, '10343.34', 14, '10/15/2021', '7/30/2021', 1);
+insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (15, '11388.11', 15, '10/20/2020', '12/29/2020', 1);
+insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (16, '3339.99', 16, '11/16/2020', '12/7/2020', 1);
+insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (17, '1969.87', 17, '12/11/2020', '5/31/2021', 1);
+insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (18, '14301.74', 18, '3/29/2021', '9/29/2021', 1);
+insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (19, '15476.54', 19, '6/18/2021', '3/11/2021', 1);
+insert into Shop_carts (id, price, user_id, created_at, buy_date, quantity) values (20, '24550.47', 20, '12/1/2020', '9/8/2021', 1);
 
 
 
-insert into Available_stocks (id, Small, Medium, Large, Extra_large, XXLarge) values (1. 2, 3, 4, 2, 2);
-insert into Available_stocks (id, Small, Medium, Large, Extra_large, XXLarge) values (0, 0, 1, 0, 2, 0);
-insert into Available_stocks (id, Small, Medium, Large, Extra_large, XXLarge) values (3, 5, 8, 6, 2, 3);
-insert into Available_stocks (id, Small, Medium, Large, Extra_large, XXLarge) values (1, 2, 3, 2, 2, 0);
-insert into Available_stocks (id, Small, Medium, Large, Extra_large, XXLarge) values (0, 0, 2, 1, 2, 0);
-insert into Available_stocks (id, Small, Medium, Large, Extra_large, XXLarge) values (2, 3, 3, 4, 2, 1);
-insert into Available_stocks (id, Small, Medium, Large, Extra_large, XXLarge) values (2, 2, 4, 2, 2, 1);
-insert into Available_stocks (id, Small, Medium, Large, Extra_large, XXLarge) values (0, 2, 1, 2, 2, 2);
-insert into Available_stocks (id, Small, Medium, Large, Extra_large, XXLarge) values (3, 4, 3, 4, 2, 3);
-insert into Available_stocks (id, Small, Medium, Large, Extra_large, XXLarge) values (0, 0, 0, 1, 2, 0);
-insert into Available_stocks (id, Small, Medium, Large, Extra_large, XXLarge) values (1, 2, 3, 2, 2, 1);
-insert into Available_stocks (id, Small, Medium, Large, Extra_large, XXLarge) values (0, 0, 0, 1, 2, 0);
-insert into Available_stocks (id, Small, Medium, Large, Extra_large, XXLarge) values (2, 2, 2, 2, 2, 2);
-insert into Available_stocks (id, Small, Medium, Large, Extra_large, XXLarge) values (0, 0, 1, 1, 2, 0);
-insert into Available_stocks (id, Small, Medium, Large, Extra_large, XXLarge) values (0, 2, 1, 1, 2, 2);
-insert into Available_stocks (id, Small, Medium, Large, Extra_large, XXLarge) values (2, 3, 2, 3, 2, 2);
-insert into Available_stocks (id, Small, Medium, Large, Extra_large, XXLarge) values (1, 2, 3, 2, 2, 2);
-insert into Available_stocks (id, Small, Medium, Large, Extra_large, XXLarge) values (1, 0, 1, 0, 2, 0);
-insert into Available_stocks (id, Small, Medium, Large, Extra_large, XXLarge) values (2, 3, 2, 2, 2, 3);
-insert into Available_stocks (id, Small, Medium, Large, Extra_large, XXLarge) values (1, 1, 2, 2, 2, 1);
+insert into Available_stocks (id, Small, Medium, Large, Extra_large) values (1, 2, 3, 4, 2);
+insert into Available_stocks (id, Small, Medium, Large, Extra_large) values (2, 0, 1, 0, 2);
+insert into Available_stocks (id, Small, Medium, Large, Extra_large) values (3, 5, 8, 6, 2);
+insert into Available_stocks (id, Small, Medium, Large, Extra_large) values (4, 2, 3, 2, 2);
+insert into Available_stocks (id, Small, Medium, Large, Extra_large) values (5, 0, 2, 1, 2);
+insert into Available_stocks (id, Small, Medium, Large, Extra_large) values (6, 3, 3, 4, 2);
+insert into Available_stocks (id, Small, Medium, Large, Extra_large) values (7, 2, 4, 2, 2);
+insert into Available_stocks (id, Small, Medium, Large, Extra_large) values (8, 2, 1, 2, 2);
+insert into Available_stocks (id, Small, Medium, Large, Extra_large) values (9, 4, 3, 4, 2);
+insert into Available_stocks (id, Small, Medium, Large, Extra_large) values (10, 0, 0, 1, 2);
+insert into Available_stocks (id, Small, Medium, Large, Extra_large) values (11, 2, 3, 2, 2);
+insert into Available_stocks (id, Small, Medium, Large, Extra_large) values (12, 0, 0, 1, 2);
+insert into Available_stocks (id, Small, Medium, Large, Extra_large) values (13, 2, 2, 2, 2);
+insert into Available_stocks (id, Small, Medium, Large, Extra_large) values (14, 0, 1, 1, 2);
+insert into Available_stocks (id, Small, Medium, Large, Extra_large) values (15, 2, 1, 1, 2);
+insert into Available_stocks (id, Small, Medium, Large, Extra_large) values (16, 3, 2, 3, 2);
+insert into Available_stocks (id, Small, Medium, Large, Extra_large) values (17, 2, 3, 2, 2);
+insert into Available_stocks (id, Small, Medium, Large, Extra_large) values (18, 0, 1, 0, 2);
+insert into Available_stocks (id, Small, Medium, Large, Extra_large) values (19, 3, 2, 2, 2);
+insert into Available_stocks (id, Small, Medium, Large, Extra_large) values (20, 1, 2, 2, 2);
 
 
 
